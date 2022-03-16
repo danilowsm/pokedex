746e729f26812143e133e0551ea5ac1b9bd28b34
Danilo Wellington <91641146+danilowsm@users.noreply.github.com>
2022-03-16 13:32:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btnBusca = document.querySelector('#btn-busca')
 
 btnBusca.onclick = () => {
     const pokemonName = document.querySelector('#pokemon-name').value
-    
+
 
     if (!pokemonName) {
         alert('O nome ou número do Pokémon ou é obrigatório')
@@ -10,8 +10,8 @@ btnBusca.onclick = () => {
 
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`
     )
-   //https://pokeapi.co/api/v2/pokemon-species/   link descrição pokemon
-   
+
+
         .then(response => {
 
             const dados = response.data
@@ -19,30 +19,41 @@ btnBusca.onclick = () => {
             let cardPokemon = document.querySelector('#main-container')
             let avatar = document.querySelector('#avatar')
             let nomeTipo = document.querySelector('#nome-tipo')
-            let bio = document.querySelector('#bio')
             let altura = document.querySelector('#altura')
             let peso = document.querySelector('#peso')
             let numeroId = document.querySelector('#numero-id')
             let linkPerfil = document.querySelector('#link-perfil')
-           
+
 
             cardPokemon.style.display = 'block'
             avatar.src = dados.sprites.other["official-artwork"].front_default
-            nomeTipo.innerText = `${dados.name} Pokémon Tipo ${dados.types[0].type.name}`
-            altura.innerText = `Altura: ${dados.height} m`
-            peso.innerText = `Peso: ${dados.weight/10} kg`
-            bio.innerText = dados.bio
+            nomeTipo.innerText = `${dados.name} Pokémon Type ${dados.types[0].type.name}`
+            altura.innerText = `Height: ${dados.height} m`
+            peso.innerText = `Weight: ${dados.weight / 10} kg`
             numeroId.innerText = `nº${dados.id}`
             linkPerfil.href = `https://api.pokemon.com/br/pokedex/${pokemonName}`
             console.log(dados)
-    
-            
-            
+})
 
-        })
 
-        
-        
+
+    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`
+    )
+        //https://pokeapi.co/api/v2/pokemon-species/   link descrição pokemon
+
+        .then(response => {
+
+            const dados = response.data
+
+            let bio = document.querySelector('#bio')
+
+            bio.innerText = dados.flavor_text_entries[Math.floor(Math.random() * 22)].flavor_text
+            console.log(dados)
+})
+
+
+
+
 }
 
 
